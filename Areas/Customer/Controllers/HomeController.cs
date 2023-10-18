f23cf8b2b7a79ee30f37bc4e0a4dd0fa857202c0
HuynhBao <huynhbao0626@gamil.com>
2023-10-18 15:17:05 +0700

    Update

--- Areas/Customer/Controllers/HomeController.cs
+++ Areas/Customer/Controllers/HomeController.cs
@@ -2,8 +2,10 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
+
 namespace ASP_MVC.Areas.Customer.Controllers
 {
+    [Area("Customer")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
